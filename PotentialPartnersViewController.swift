ccdad430224bcf962385919815d18cd7086bf232
nwang9 <nicowang9@gmail.com>
2017-08-10 15:47:40 -0700

    linked up all view controllers

---
+++ PotentialPartnersViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PotentialPartnersViewController.swift
+//  AccountabilityPartner
+//
+//  Created by Nico on 8/10/17.
+//
+//
+
+import Foundation
+import UIKit
+import Parse
+
+class PotentialPartnersViewController: UIViewController {
+}
---
+++ ViewControllers/PostDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PostDetailsViewController.swift
+//  AccountabilityPartner
+//
+//  Created by Nico on 8/10/17.
+//
+//
+
+import Foundation
+import UIKit
+import Parse
+
+class PostDetailsViewController: UIViewController {
+}
