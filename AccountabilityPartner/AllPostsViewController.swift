7048a1f15d38bc559089a2c822e02de215e3f5d4
nwang9 <nicowang9@gmail.com>
2017-08-17 16:01:41 -0700

    changed button titles in postCells

--- AccountabilityPartner/AllPostsViewController.swift
+++ AccountabilityPartner/AllPostsViewController.swift
@@ -21,7 +21,12 @@ class AllPostsViewController: NavBarViewController, UITableViewDelegate, UITable
     @IBOutlet weak var postAuthor: UIButton!
     @IBOutlet weak var postDate: UILabel!
     
+    //arrays to organize post attributes
+    var postTitles = [String]()
+    var postAuthors = [String]()
+    var postDates = [String]()
     
+    // Brings you to the new post creation page
     @IBAction func goToNewPost(_ sender: Any) {
         let vc:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "newPost")
         self.navigationController?.pushViewController(vc,animated: true)
@@ -37,8 +42,15 @@ class AllPostsViewController: NavBarViewController, UITableViewDelegate, UITable
     // Create cell
     public func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "postCell")
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "postCell") as! PostTableViewCell
         
+        cell.postTitle.setTitle(postTitles[indexPath.row],for: .normal)
+        
+        cell.postAuthor.setTitle(postAuthors[indexPath.row], for: .normal)
+        
+        cell.postDate.text = postDates[indexPath.row]
+        
+        return cell
         
         /*cell.textLabel?.text = "1 " + item.Size + " " + item.Color + " " + item.objectType
         for groupItem in foundIds {
@@ -50,8 +62,22 @@ class AllPostsViewController: NavBarViewController, UITableViewDelegate, UITable
             }
         }
         */
-        return cell
+        
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*
+        let postQuery = PFQuery(className: "Post")
+        postQuery.findObjectsInBackground(block: { (objects, error) in
+            if let posts = objects {
+                for post in posts {
+                        allPosts += post
+                }
+            }
+        }
+    })
+    */
 
+    }
 }
